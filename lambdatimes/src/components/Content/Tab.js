1e2ee5d69a1e002cacaf5356c5a36da85dbf48b1
Reyaad Rafeek <reyaad510@gmail.com>
2019-03-23 12:59:42 -0400

    Filter method working

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,14 +1,11 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
-      if it is not it should just be 'tab'*/
+
   return (
     <div
-      // className={''}
       className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
-      onClick={() => {props.selectTabHandler(props.tab) }}
+      onClick={() => { props.selectTabHandler(props.tab) }}
     >
       {props.tab.toUpperCase()}
     </div>
